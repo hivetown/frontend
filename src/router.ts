25334d0f45a35b47d5b826f50e6f8e8b110ed263
Madalena Rodrigues <108576457+Mad0cha@users.noreply.github.com>
2023-03-04 10:39:04 +0000

    alguns testes de juncao entre backend e frontend

--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import Home from "./views/Home.vue";
 import About from "./views/About.vue";
+import CategoriasProdutos from "./views/CategoriasProdutos.vue";
+import Testes from "./views/Testes.vue";
 
 const routes = [
     {
@@ -13,6 +15,22 @@ const routes = [
         path: "/about",
         name: "About",
         component: About,
+    },
+    {
+        path: "/produtos",
+        name: "Produtos",
+        component: CategoriasProdutos,
+    },
+    {
+        path: "/products",
+        name: "Products",
+        component: Testes,
+    },
+
+    {
+        path: "/testes",
+        name: "Testes",
+        component: Testes,
     },
 ];
 
