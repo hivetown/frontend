226c843f4a683243487af364afcc3beb07530bdd
BMGonzalez24 <46931446+BMGonzalez24@users.noreply.github.com>
2023-03-14 22:01:04 +0000

    Criado e desenvolvido (quase concluido) o frontend do Cart e do seu respetivo componente, CartItem.

---
+++ src/components/CartItem.vue
@@ -0,0 +1,47 @@
+<template>
+   <div class="d-flex gap-4">
+       <router-link to="/produto">
+           <b-card class="prod-card">
+               <span class="position-absolute top-0 end-0">
+               </span>
+               <!-- <img src="https://placehold.jp/150x150.png" class="square-image"> -->
+               <img src="mac.png" class="square-image">
+           </b-card>
+       </router-link>
+       <b-card-text class="w-100" style="background-color: yellow;">
+           <div>
+                <div class="d-flex gap-2"></div>
+                    <h5>Apple MacBook</h5>
+                    <p class="grey-txt">16GB RAM | 1TB | Placa gráfica</p>
+                    <div class="d-flex gap-2">
+                        <h4 class="mb-3">999€</h4>
+                    <!-- O <p> seguinte apenas é adicionado quando o item tem outro preço e está agor em promoção -->
+                    <p class="mt-1 grey-txt text-decoration-line-through">1025€</p>
+                    <div class="d-flex ms-auto justify-content-end">
+                        <button type="button" class="btn btn-outline-secondary circle-btn" v-b-tooltip.hover title="Remover do carrinho">
+                            <i class="bi bi-x-lg"></i>
+                        </button>
+                    </div>
+               </div>
+           </div>
+       </b-card-text>
+   </div>
+
+
+  <div>
+    <label for="demo-sb">Spin Button</label>
+    <b-form-spinbutton id="demo-sb" v-model="value" min="1" max="100"></b-form-spinbutton>
+    <p>Value: {{ value }}</p>
+  </div>
+
+</template>
+
+<script>
+export default {
+    data() {
+      return {
+        value: 50
+      }
+    }
+  }
+</script>
--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,3 +1,110 @@
-<template>
+<template >
+  
+    <div class="parent">
+      <div style="text-align: center;">
+          <button @click="goBack" type="button" class="btn btn-outline-secondary btn-sm" style="float:">
+            Voltar
+          </button>
+      </div>
+      
+      <table class="table table-responsive-md">
+          <tr>
+              <td class="left-column">
+              <div>
+                  X items no carrinho
+
+                  <div style="height:2vh"></div>
+
+                  <CartItem></CartItem>
+                  <CartItem></CartItem>
+              </div>
+              </td>
+
+              <td class="right-column">
+              <div>
+                <h3>Rever a comprar</h3>
+                  <div style="height:2vh"></div>
+
+                  <p style="text-align: justify;">
+                    Sub-total: <span class="checkout">0,00€</span><br>
+                    Entrega: <span class="checkout">0,00€</span><br>
+                  </p>
+
+                  <div style="height:5vh;"></div>
+
+                  <button type="button" class="btn btn-outline-secondary btn-sm checkout" >
+                    Aplicar
+                  </button>
+
+                  <div style="height:10vh;"></div>
+
+                  <p>
+                    Total: <span class="checkout">0,00€</span><br>
+                  </p>
+                  <div style="text-align: center;">
+                    <button type="button" class="btn btn-outline-secondary btn-sm" style="text-align: center;">
+                      Finalizar a compra
+                    </button>
+                  </div>
+              </div>
+          </td>
+          </tr>
+      </table>
+    </div>
     
 </template>
+
+
+
+<style>
+.container {
+    display: flex;
+  align-items: center;
+}
+
+table {
+  border: 1px solid #aaa;
+  width: 100%;
+  border-collapse: collapse;
+}
+
+@media (max-width: 800px) {
+  tr {
+    display: block;
+  }
+
+  td {
+    border: 1px solid black;
+    padding-left: 10px;
+    padding-right: 10px;
+    display: inline-block;
+    word-break: break-all;
+  }
+}
+
+.left-column {
+  width: 55%;
+}
+
+.right-column {
+  width: 25%;
+}
+
+.checkout {
+  float: right;
+  text-align: center;
+}
+</style>
+
+<script setup lang="ts"> 
+import CartItem from "@/components/CartItem.vue";
+</script>
+
+<script lang="ts">
+    export default {
+    methods: {
+        goBack() {window.history.back();}
+    }
+    };
+</script>
+
