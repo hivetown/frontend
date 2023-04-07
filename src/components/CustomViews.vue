4e23853396bf1f62ad07930ef6cc8f10ae01fa53
Madalena <madalenalucasrodrigues@gamil.com>
2023-04-07 13:17:28 +0100

    pequenas alterações nas views da página dos produtos

--- src/components/CustomViews.vue
+++ src/components/CustomViews.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="d-flex parent" style="">
-        <div class="w-25 d-flex align-items-center" style="background-color:;">
-            <!-- TODO preencher automaticamente -->
+    <div class="d-flex parent">
+        <!-- Produtos -->
+        <div class="w-25 d-flex align-items-center">
             <p><span>{{ items }}</span> produtos encontrados</p>
         </div>
 
@@ -9,14 +9,15 @@
         <div class="w-25" style=""></div>
 
         <div class="w-50 d-flex justify-content-end align-items-center" style="gap:2vh;">
-
+            <!-- Views -->
             <div class="custom-views d-flex justify-content-center align-items-center" style="width:10%;">
+                <i class="bi bi-grid selected" v-b-tooltip.hover title="Vista em grelha"></i>
                 <i class="bi bi-list-ul" v-b-tooltip.hover title="Vista em lista"></i>
-                <i class="bi bi-grid" v-b-tooltip.hover title="Vista em grelha 2x2"></i>
-                <!-- <i class="bi bi-grid-3x3-gap" v-b-tooltip.hover title="Vista em grelha 3x3"></i> -->
             </div>
 
-            <div class="d-flex" style="">
+            <!-- Mostrar -->
+            <div class="d-flex">
+                <!-- Quantidade -->
                 <div class="views-btn">
                     <b-dropdown id="dropdown-1" :text="'Mostrar: ' + currentPageSize" class="m-md-2">
                         <b-dropdown-item @click="setCurrentPageSize(12)">12</b-dropdown-item>
@@ -24,7 +25,7 @@
                         <b-dropdown-item @click="setCurrentPageSize(48)">48</b-dropdown-item>
                     </b-dropdown>
                 </div>
-
+                <!-- Ordem -->
                 <div class="views-btn">
                     <b-dropdown id="dropdown-1" :text="'Mostrar: ' + order" class="m-md-2">
                         <b-dropdown-item @click="order = 'Avaliação'">Avaliação</b-dropdown-item>
@@ -32,7 +33,6 @@
                         <b-dropdown-item @click="order = 'Preço (descendente)'">Preço (descendente)</b-dropdown-item>
                     </b-dropdown>
                 </div>
-
             </div>
         </div>
     </div>
@@ -45,17 +45,17 @@
 }
 
 .custom-views i {
-    color: #164A41;
+    color: #919191;
     font-size: 2.5vh;
     cursor: pointer;
 }
 
-/* .views-btn .btn{
-        background-color: white !important;
-        color:#5A5A5A;
-        border: solid 1px #164A41;
-    } */
+.selected{
+    color: #164A41 !important;
+}
 </style>
+
+
 <script>
 export default {
     props: {
