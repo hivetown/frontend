a0449820d977442086063be3f8d9c568603f2ee8
Lucas Pinto <github@lucks.pt>
2023-04-16 22:39:29 +0100

    feat: base items interface

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -22,3 +22,31 @@ export interface Producer {
     vat: string;
     products?: Product[];
 }
+
+export interface Consumer {
+    id: number;
+    name: string;
+    email: string;
+    phone: string;
+    vat: string;
+}
+
+export interface CreateConsumer {
+    name: string;
+    phone: string;
+    vat: string;
+}
+
+export interface ApiRequest {
+    headers?: Record<string, string>;
+    params?: Record<string, string>;
+    data?: any;
+}
+
+export interface BaseItems<T> {
+    items: T[];
+    totalItems: number;
+    totalPages: number;
+    page: number;
+    pageSize: number;
+}
