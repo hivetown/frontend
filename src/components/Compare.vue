c4b35cb226de11f613683fc88e7c7bddde513f77
Madalena <madalenalucasrodrigues@gamil.com>
2023-04-29 20:34:36 +0100

    Comparaçao de produtos - mostrar fields de ambos

--- src/components/Compare.vue
+++ src/components/Compare.vue
@@ -32,7 +32,18 @@
             <div class="mt-3 d-flex align-items-center spec-category-text">
                 <h5 class="p-3">Características</h5>
             </div>
-        
+            
+            <div v-for="field in fieldsTotais" style="background-color: pink; border-bottom: solid 1px black;">
+                Produto:{{ field[0] }}<br>
+                Fields: 
+                <div v-for="fs in field[1]" class="mb-4">
+                    <!-- Prod x: {{ fs }} -->
+                    <div v-for="f in fs">
+                        {{ f }}
+                    </div>
+                </div>
+            </div>
+            <!-- {{ fieldsTotais }} -->
             <div class="d-flex gap-5">
                 <!-- <div>
                     <div v-for="(category, index) in product1Categories.items" :key="index">
@@ -93,6 +104,9 @@ export default defineComponent({
             product2Categories: {},
             fields1: [],
             fields2: [],
+
+            categoriasTotais: {},
+            fieldsTotais: {}
         };
     },
     props: {
@@ -111,6 +125,7 @@ export default defineComponent({
         console.log("foi recarregado")
         const productSpec1 = await fetchProduct(this.product1Id);
         this.productSpec1 = productSpec1.data;
+        // console.log("Dados do 1: " + JSON.stringify(this.productSpec1))
         this.product1Img = productSpec1.data.images[0];
         const productSpec2 = await fetchProduct(this.product2Id);
         this.productSpec2 = productSpec2.data;
@@ -121,22 +136,79 @@ export default defineComponent({
         // TODO - filtrar iguais e diferenças em destaque
         const product1Categories = await fetchProductCategories(this.product1Id);
         this.product1Categories = product1Categories.data;
+        // console.log("Categorias do 1: " + JSON.stringify(this.product1Categories))
 
         const product2Categories = await fetchProductCategories(this.product2Id);
         this.product2Categories = product2Categories.data;
+        // console.log("Categorias do 2: " + JSON.stringify(this.product2Categories))
 
         try {
             for (const categoria of this.product1Categories.items) {
+                if (!(categoria.id in this.categoriasTotais)) {
+                    this.categoriasTotais[categoria.id] = [1];
+                }else{
+                    this.categoriasTotais[categoria.id].push([1]);
+                }
                 const response = await fetchProductCategoriesFields(this.product1Id, categoria.id);
-                this.fields1.push(response.data.items);
+                this.fields1.push([categoria.id,response.data.items]);
             }
+            // console.log("Fields do 1: " + JSON.stringify(this.fields1))
             for (const categoria of this.product2Categories.items) {
+                // Junta as categorias dos dois produtos que são iguais
+                if (!(categoria.id in this.categoriasTotais)) {
+                    this.categoriasTotais[categoria.id] = [2];
+                } else {
+                    this.categoriasTotais[categoria.id].push([2]);
+                }
                 const response = await fetchProductCategoriesFields(this.product2Id, categoria.id);
-                this.fields2.push(response.data.items);
+                this.fields2.push([categoria.id, response.data.items]);
             }
+            console.log("Fields do 2: " + JSON.stringify(this.fields2))
         } catch (error) {
             console.log("Erro: " + error);
         }
+        // console.log("categoriasTotais: " + JSON.stringify(this.categoriasTotais))
+        // Para cada categoria das categorias totais, ver os fields dela por produto
+        for (const [cat, prod] of Object.entries(this.categoriasTotais)) {
+            // console.log(`cat: ${cat}, prod: ${prod}`);
+            for (const field of this.fields1) {
+                if (field[0] == cat) {
+                     if (!(cat in this.fieldsTotais)) {
+                        this.fieldsTotais[cat] = [[1], [field[1]]];
+                    } else {
+                        this.fieldsTotais[cat][0].push(1)
+                        this.fieldsTotais[cat][1].push(field[1])
+                    }
+                    // console.log(field[0], JSON.stringify(field[1]));
+                }
+                // console.log(field[0], JSON.stringify(field[1]));
+            }
+            for (const field of this.fields2) {
+                if (field[0] == cat) {
+                    if (!(cat in this.fieldsTotais)) {
+                        this.fieldsTotais[cat] = [[2], [field[1]]];
+                    } else {
+                        this.fieldsTotais[cat][0].push(2)
+                        // for(const f of this.fieldsTotais[cat][1]){
+                        //     console.log("f: "+JSON.stringify(f))
+                        //     if(!(this.fieldsTotais[cat][1].includes(f.id))){
+                        //         this.fieldsTotais[cat][1].push([field[1]])
+                        //     }
+                        // }
+                        this.fieldsTotais[cat][1].push(field[1])
+                        // console.log("meh "+this.fieldsTotais[cat])
+                        // this.fieldsTotais[cat][0].unshift(2);
+                    }
+                    // console.log(field[0], JSON.stringify(field[1]));
+                }
+                // console.log(field[0], JSON.stringify(field[1]));
+            }
+        }
+        for (const [cat, prod] of Object.entries(this.fieldsTotais)) {
+            // console.log(`cat: ${cat}, prod: ${prod[0]}` + ` fields: ${JSON.stringify(prod[1])}`);
+            // console.log(`cat: ${cat}, prod: ${JSON.stringify(prod)}`);
+        }
+        // console.log("fieldsTotais: " + JSON.stringify(this.fieldsTotais))
     },
     components: { ProductCard }
 });
