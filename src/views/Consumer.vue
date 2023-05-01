c8f7a6e3887745059de21220f4e0122f31c49b95
MatiSilva01 <matildeferreirasilva@gmail.com>
2023-05-01 17:11:58 +0100

    confirmacao desativar conta

--- src/views/Consumer.vue
+++ src/views/Consumer.vue
@@ -1,8 +1,10 @@
 <template>
+  <div id="container">
+    <div id="titulo">
     <h1>Consumidor {{  $route.params.id }}</h1>
-    
+    </div>
 <div class="cart">
-  <b-card
+  <b-card id="b-card"
     title="nome"
     img-src="https://picsum.photos/600/300/?image=25"
     img-alt="Image"
@@ -20,11 +22,14 @@
       <br>
     </b-card-text>
 
-    <b-button href="#" variant="primary">Desativar conta</b-button>
+    <b-button href="#" variant="secondary" @click="showCancelDialog"><span style="color:white;">Desativar conta</span></b-button>
   </b-card>
-</div>
+</div> 
+  </div>
 </template>
 <script lang="ts">
+import Swal from 'sweetalert2';
+
 import {getConsumerId} from '../api/consumers'
 import { onMounted, ref} from "vue";
 import { Consumer } from "../types/interfaces";
@@ -47,16 +52,55 @@ export default {
 
     return { users };
   },
+  methods: {
+    showCancelDialog(): void {
+      Swal.fire({
+  title: 'Dasativar conta?',
+  text: "A conta pode voltar a ser ativada.",
+  icon: 'warning',
+  showCancelButton: true,
+  cancelButtonText: "Cancelar",
+  confirmButtonColor: '#d33',
+  cancelButtonColor: '#3085d6' ,
+  confirmButtonText: 'Sim, desativar!'
+}).then((result) => {
+  if (result.isConfirmed) {
+    Swal.fire(
+      'Desativa!',
+      'A conta encontra-se desativada.',
+      'success'
+    )
+  }
+})
+  }
+
+  }
 };
 
 </script>
 <style scoped>
 h1 {
-    text-align: center;
-    font-family: 'DM Serif Display';
+  text-align: center;
+  font-family: 'DM Serif Display';
 }
+
 .cart {
-    width: 800px;
-    margin-left: 440px;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+#b-card{
+  background-color: rgb(221, 229, 237)!important;
+}
+
+/* estilos para telas com largura menor ou igual a 600px */
+@media (max-width: 600px) {
+
+  .cart {
+    width: 350px;
+
+  }
 }
 </style>
