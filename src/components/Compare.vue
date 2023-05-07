80e3d1a91e81411fd69cc5d9b1bb08f505e0fca1
Madalena <madalenalucasrodrigues@gamil.com>
2023-05-07 16:37:01 +0100

    adição de um componente para as product specs

--- src/components/Compare.vue
+++ src/components/Compare.vue
@@ -20,43 +20,9 @@
                             :productId="product2Id" 
                             :productPrice="[productSpec2.minPrice, productSpec2.maxPrice]" />
         </div>
-
         <!-- Características dos produtos a ser comparados -->
         <div class="spec-to-compare">
-            <div class="mt-3 mb-3 d-flex align-items-center spec-category-text">
-                <h5 class="p-3">Características</h5>
-            </div>
-            <div>
-                <div class="">
-                    <!-- Field name -->
-                    <div v-for="(field, index) in fieldsTotais" :key="index" class="mb-3 parent" style="border-bottom: 1px solid #eeeeee;">
-                        <div>
-                            <div class="d-flex" style="gap:12vh;" >
-                                <div v-for="(fs, fsIndex) in field[1]" :key="fsIndex" class="mb-4 d-flex gap-4">
-                                    <div v-if="fsIndex === 0">
-                                        <div v-for="f in fs" style="background-color: ; width: 25vh;">
-                                            <span style="font-weight: bold">{{ f.field.name }}:</span>
-                                        </div>
-                                    </div>
-                                    <div class="" style="background-color: ; width: 25vh;">
-                                        <div v-for="f in fs" :key="f.field.id" class="d-flex">
-                                            <div style="background-color: ; min-width: 25vh;">
-                                                {{ f.value }}
-                                                {{ f.field.unit }}
-                                            </div>
-                                            <div v-if="field[0].length < 2">
-                                                <div style="background-color: ; width: 25vh; display: flex; justify-content: center;">
-                                                    <span>-</span>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <ProductSpec :fieldsTotais="fieldsTotais"></ProductSpec>
         </div>
     </div>
 </template>
@@ -72,6 +38,7 @@
 <script setup lang="ts">
 // Componentes
     import ProductCard from "@/components/ProductCard.vue";
+    import ProductSpec from "@/components/ProductSpec.vue";
 
     // API
     import { fetchProduct, fetchProductCategories, fetchProductCategoriesFields } from "@/api";
