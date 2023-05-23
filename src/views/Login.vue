7ee2ea025b412879258fb2384d9aa0431726e65f
Lucas Pinto <github@lucks.pt>
2023-05-23 19:29:55 +0100

    chore: improve login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,115 +1,137 @@
 <template>
-  <main class="login">
-    <section class="forms">
-      <form class="login" @submit.prevent="login">
-        <h2>Login</h2>
-        <div class="grid-item">
-          <input type="email" placeholder="Email" v-model="loginForm.email" />
-        </div>
-        <div class="grid-item">
-          <input
-            type="password"
-            placeholder="Palavra passe"
-            v-model="loginForm.password"
-          />
-        </div>
+  <div id="full" class="grid align-items-center justify-content-center">
+    <Card class="3">
+      <template #title> Entrar </template>
 
-        <div class="grid-item">
-          <input type="submit" value="Login" />
-        </div>
+      <template #content>
+        <VeeForm @submit="login" class="flex flex-column gap-3">
+          <div class="flex flex-column gap-2">
+            <label for="formEmail"
+              >Email <span class="text-red-500">*</span></label
+            >
+            <span class="p-input-icon-left">
+              <i class="pi pi-envelope" />
+              <InputText
+                input-id="formEmail"
+                v-model="formValues.email"
+                :input-class="{ 'p-invalid': formErrors.email }"
+                aria-describedby="emailError"
+                placeholder="Email"
+              />
+            </span>
 
-        <p>
-          Não tem uma conta?
-          <router-link to="/registration">Registar</router-link>
-        </p>
-      </form>
-    </section>
-  </main>
+            <small v-if="formErrors.email" class="p-error" id="emailError">{{
+              formErrors.email
+            }}</small>
+          </div>
+
+          <div class="flex flex-column gap-2">
+            <label for="formPassword"
+              >Password <span class="text-red-500">*</span></label
+            >
+            <span class="p-input-icon-left">
+              <i class="pi pi-lock" />
+              <InputText
+                input-id="formPassword"
+                type="password"
+                v-model="formValues.password"
+                :input-class="{ 'p-invalid': formErrors.password }"
+                aria-describedby="passwordError"
+                placeholder="Password"
+              />
+            </span>
+
+            <small v-if="formErrors.password" class="p-error" id="passwordError"
+              ><span v-html="formErrors.password"
+            /></small>
+          </div>
+
+          <PrimeButton type="submit" label="Entrar" :loading="loading" />
+
+          <small
+            >Não tenho conta ainda!
+            <RouterLink to="/register">Registar</RouterLink></small
+          >
+        </VeeForm>
+      </template>
+    </Card>
+  </div>
 </template>
 
 <script lang="ts">
+import Card from 'primevue/card';
+import { Form as VeeForm, useField, useForm } from 'vee-validate';
 import { useStore } from '@/store';
+import InputText from 'primevue/inputtext';
+import PrimeButton from 'primevue/button';
 import { ref } from 'vue';
 export default {
+  components: {
+    Card,
+    VeeForm,
+    InputText,
+    PrimeButton,
+  },
   setup() {
-    const loginForm = ref({
-      email: '',
-      password: '',
+    const {
+      values: formValues,
+      errors: formErrors,
+      handleSubmit,
+    } = useForm({
+      initialValues: {
+        email: '',
+        password: '',
+      },
     });
+
+    useField('email', (value: string) => {
+      if (!value) {
+        return 'O email é obrigatório';
+      }
+
+      if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value)) {
+        return 'O email deve ser válido';
+      }
+
+      return true;
+    });
+
+    useField('password', (value: string) => {
+      if (!value) {
+        return 'A password é obrigatória';
+      }
+
+      if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/.test(value)) {
+        return 'A password deve conter pelo menos<br/><ul><li>8 caracteres</li><li>uma letra maiúscula</li><li>uma letra minúscula</li><li>um número</li></ul>';
+      }
+
+      return true;
+    });
+
     const store = useStore();
-    const login = () => {
-      return store.dispatch('login', loginForm.value);
-    };
+    const loading = ref(false);
+    const login = handleSubmit(async () => {
+      loading.value = true;
+      await store.dispatch('login', formValues);
+      loading.value = false;
+    });
+
     return {
-      loginForm,
+      loading,
+      formValues,
       login,
+      formErrors,
     };
   },
 };
 </script>
 
 <style scoped>
-.forms {
-  display: flex;
-  min-height: 100vh;
-}
-form {
-  flex: 1 1 0%;
-  padding: 3rem 1rem 1rem;
-}
-form.register {
-  color: rgb(0, 0, 0);
-}
-h2 {
-  font-size: 2rem;
-  text-transform: uppercase;
-  margin-bottom: 2rem;
-}
-input {
-  border: none;
-  outline: none;
-  background: none;
-  display: block;
-  width: 100%;
-  max-width: 400px;
-  margin: 0 auto;
-  font-size: 1.5rem;
-  margin-bottom: 2rem;
-  padding: 0.5rem 0rem;
-}
-input:not([type='submit']) {
-  opacity: 0.8;
-  transition: 0.4s;
-}
-input:focus:not([type='submit']) {
-  opacity: 1;
-}
-input::placeholder {
-  color: inherit;
-}
-form.register input:not([type='submit']) {
-  color: #2c3e50;
-  border-bottom: 2px solid #2c3e50;
-  margin-bottom: 10%;
-}
-form.login input:not([type='submit']) {
-  color: #2c3e50;
-  border-bottom: 2px solid #2c3e50;
-}
-input[type='submit'] {
-  background: #2c3e50;
-  color: #fff;
-  border: 2px solid #2c3e50;
-  cursor: pointer;
-  transition: 0.4s;
-  font-weight: 700;
-  padding: 1rem 2rem;
-  border-radius: 0.5rem;
-  text-transform: uppercase;
-  margin-bottom: 1rem;
-}
-input[type='submit']:hover {
-  background: #fff;
-  color: #2c3e50;
+#full {
+  height: 80vh;
+  width: 99vw;
+  background-image: url('/c1.svg');
+  background-size: cover;
+  background-repeat: no-repeat;
 }
 </style>
