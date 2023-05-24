4a1827fb46b16c008a5743f5a27936d18976347e
Matilde Silva <82830782+MatiSilva01@users.noreply.github.com>
2023-05-24 21:42:46 +0100

    Update src/api/consumers.ts
    Co-authored-by: Lucas Pinto <github@lucks.pt>

--- src/api/consumers.ts
+++ src/api/consumers.ts
@@ -6,7 +6,3 @@ export const createConsumer = async (consumer: CreateConsumer) =>
 
 export const createConsumerAddress = (id: number, address: Address) =>
     api.post(`/consumers/${id}/addresses`, address);
-
-// consumer is consumer or supplier
-export const postConsumer = (consumer: CreateConsumer) => 
-    api.post("/consumers", consumer);
