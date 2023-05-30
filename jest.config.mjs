dda8613664e351dea420aa5deefe016b420ffb67
Lucas Pinto <github@lucks.pt>
2023-05-30 14:09:02 +0100

    chore: fix and add dummy test

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,14 +1,12 @@
 /** @type {import('@jest/types').Config.InitialOptions} */
 const config = {
-	displayName: 'unit test',
-	preset: 'ts-jest',
-	testMatch: ['<rootDir>/tests/**/*.test.ts'],
-	collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-	globals: {
-		'ts-jest': {
-			tsconfig: '<rootDir>/tests/tsconfig.json'
-		}
-	}
+  displayName: 'unit test',
+  preset: 'ts-jest',
+  testMatch: ['<rootDir>/tests/**/*.test.ts'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
 };
 
 export default config;
