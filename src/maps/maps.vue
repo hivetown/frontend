2bc93ab40f776e0e92f8cb6492d0a01e44ae97af
Pedro Almeida <fc56897@alunos.fc.ul.pt>
2023-06-09 16:09:54 +0100

    feat: maps with specific location

--- src/maps/maps.vue
+++ src/maps/maps.vue
@@ -4,60 +4,11 @@
     access-token="pk.eyJ1IjoiaGl2ZXRvd24iLCJhIjoiY2xpbmI5NmJ1MHF5cjNtcGpsMnZibjdtZSJ9.76vplIvkyBQogSglqzp0Uw"
     map-style="mapbox://styles/mapbox/streets-v11"
     :center="mapCenter"
-    :zoom="1"
+    :zoom="14"
   />
 </template>
 
-<!-- <script setup lang="ts">
-import { ref } from 'vue';
-
-// Using 'any' type for MapboxMap
-let MapboxMap: any = null;
-import('@studiometa/vue-mapbox-gl').then((module) => {
-  MapboxMap = module.MapboxMap;
-});
-
-const mapCenter = ref([0, 0]);
-
-onMounted(async () => {
-  mapboxgl.accessToken = 'YOUR_MAPBOX_ACCESS_TOKEN';
-  const map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11',
-    center: mapCenter.value,
-    zoom: 1,
-  });
-
-  try {
-    const mapData = await fetchMapData();
-    // Process and use the map data as needed
-    console.log(mapData);
-  } catch (error) {
-    console.error('Error fetching map data:', error);
-  }
-});
-
-async function fetchMapData() {
-  try {
-    const response = await axios.get('/api/map-data');
-    return response.data; // Assuming the map data is returned in the response's data property
-  } catch (error) {
-    throw new Error('Error fetching map data');
-  }
-}
-
-function onMounted(arg0: () => Promise<void>) {
-throw new Error('Function not implemented.');
-}
-</script>
-
-<style scoped>
-#map {
-  height: 400px;
-}
-</style> -->
-
-<script lang="ts">
+<!-- <script lang="ts">
 import { ref, onMounted } from 'vue';
 import { MapboxMap } from '@studiometa/vue-mapbox-gl';
 import axios from 'axios';
@@ -86,8 +37,37 @@ export default {
     };
   },
 };
-</script>
+</script> -->
+<script lang="ts">
+import { onMounted, PropType } from 'vue';
+import { fetchMapForUnit } from './maps';
 
+export default {
+  props: {
+    selectedUnit: {
+      type: Object as PropType<any>,
+      required: true,
+    },
+  },
+  setup(props) {
+    onMounted(async () => {
+      try {
+        const mapData = await fetchMapForUnit(
+          props.selectedUnit.latitude, // Pass the latitude value of the selected unit
+          props.selectedUnit.longitude // Pass the longitude value of the selected unit
+        );
+        // Process and use the map data as needed
+        console.log(mapData);
+      } catch (error) {
+        console.log('selectedUnit', props.selectedUnit);
+        console.error('Error fetching map data:', error);
+      }
+    });
+
+    return {};
+  },
+};
+</script>
 <style scoped>
 #map {
   height: 400px;
