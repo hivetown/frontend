03fec5d4d964079cfdbad0e720e617fb134d8668
PedrosGuitarLegend <91616306+PedrosGuitarLegend@users.noreply.github.com>
2023-06-14 14:11:20 +0100

    Update src/api/units.ts
    Co-authored-by: Lucas Pinto <github@lucks.pt>

--- src/api/units.ts
+++ src/api/units.ts
@@ -1,9 +1,6 @@
 import { ProductionUnits } from '../types/interfaces';
 import { api } from './_base';
 
-// export const fetchAllProducts = (id: number) =>
-//     api.get<ProducerProducts[]>(`/producers/${id}/products`);
-
 export const fetchAllUnits = (
     id: number,
     page: number,
