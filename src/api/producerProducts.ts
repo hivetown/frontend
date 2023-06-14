de407c5259f4936684169052c5526dbf3011e4c9
PedrosGuitarLegend <91616306+PedrosGuitarLegend@users.noreply.github.com>
2023-06-14 14:10:39 +0100

    Update src/api/producerProducts.ts
    Co-authored-by: Lucas Pinto <github@lucks.pt>

--- src/api/producerProducts.ts
+++ src/api/producerProducts.ts
@@ -1,9 +1,6 @@
 import { ProducerProducts } from '../types/interfaces';
 import { api } from './_base';
 
-// export const fetchAllProducts = (id: number) =>
-//     api.get<ProducerProducts[]>(`/producers/${id}/products`);
-
 export const fetchAllProducts = (
     id: number,
     page: number,
