c883268ffbb302d312b33211587800281fe7d59c
Madalena <madalenalucasrodrigues@gamil.com>
2023-06-15 22:17:13 +0100

    feat: mobile mode and improvements

--- src/components/CategoryCard.vue
+++ src/components/CategoryCard.vue
@@ -1,6 +1,4 @@
 <template>
-  <!-- TODO fazer com que a imagem e o texto sejam automáticos -->
-  <!-- Todas as imagens têm de ser do mesmo tamanho -->
   <b-card class="category-card" :img-src="image" img-alt="Imagem de exemplo">
     <div class="card-img-overlay d-flex justify-content-center text-white">
       <h5 class="card-title text-center">{{ title }}</h5>
@@ -10,13 +8,32 @@
 
 <style>
 .category-card {
-  width: 17vh;
+  min-width: 15vh !important;
+  height: 20vh !important;
+}
+
+.category-card h5 {
+  margin-top: 1vh;
+  color: white;
 }
 
 .category-card img {
   border-radius: 1.3em !important;
-  max-height: 22vh;
+  /* max-height: 22vh; */
+  width: 100%;
+  height: 100%;
   box-shadow: rgba(0, 0, 0, 0.05) 0px 0px 0px 1px;
+}
+
+@media (max-width: 767px) {
+  .category-card {
+    min-width: 10vh !important;
+    height: 14vh !important;
+  }
+
+  .category-card h5 {
+    margin-top: 0.3vh !important;
+  }
 }
 </style>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,21 +4,40 @@
   <div id="categories" class="parent py-3">
     <div class="title d-flex justify-content-between align-items-center mb-5">
       <h3 class="grey-txt">Categorias Populares</h3>
-      <button type="button" class="btn btn-outline-secondary btn-sm">
+      <!-- <button type="button" class="btn btn-outline-secondary btn-sm">
         Ver todas
-      </button>
+      </button> -->
     </div>
-    <div class="d-flex gap-4 justify-content-center">
-      <CategoryCard title="Tecnologia" image="categories/tech.png" />
-      <CategoryCard title="Arte" image="categories/art.png" />
-      <CategoryCard title="Moda" image="categories/clothes.png" />
-      <CategoryCard title="Desporto" image="categories/sport.png" />
-      <CategoryCard title="Animais" image="categories/pets.png" />
-      <CategoryCard title="Tecnologia" image="categories/tech.png" />
-      <CategoryCard title="Arte" image="categories/art.png" />
-      <CategoryCard title="Moda" image="categories/clothes.png" />
-      <CategoryCard title="Desporto" image="categories/sport.png" />
-      <CategoryCard title="Animais" image="categories/pets.png" />
+    <div class="d-flex cards-mobile">
+      <!-- TODO - routes para as categorias -->
+      <router-link to="/products">
+        <CategoryCard title="Eletrónica" image="categories/tech.png" />
+      </router-link>
+      <router-link to="/products">
+        <CategoryCard title="Arte" image="categories/art.png" />
+      </router-link>
+      <router-link to="/products">
+        <CategoryCard title="Moda" image="categories/clothes.png" />
+      </router-link>
+      <router-link to="/products">
+        <CategoryCard title="Desporto" image="categories/sport.png" />
+      </router-link>
+      <!-- <CategoryCard title="Animais" image="categories/pets.png" /> -->
+      <router-link to="/products">
+        <CategoryCard title="Jardim" image="categories/garden.png" />
+      </router-link>
+      <router-link to="/products">
+        <CategoryCard title="Bebé" image="categories/baby.png" />
+      </router-link>
+      <router-link to="/products">
+        <CategoryCard title="Beleza" image="categories/beauty.png" />
+      </router-link>
+      <router-link to="/products">
+        <CategoryCard title="Casa" image="categories/house.png" />
+      </router-link>
+      <router-link to="/products">
+        <CategoryCard title="Livros" image="categories/books.png" />
+      </router-link>
     </div>
   </div>
 
@@ -29,11 +48,10 @@
         <router-link to="/products">Ver todos</router-link>
       </button>
     </div>
-    <div class="d-flex justify-content-center" style="gap: 12vh">
-      <!-- <ProductCard></ProductCard>
-      <ProductCard></ProductCard>
-      <ProductCard></ProductCard>
-      <ProductCard></ProductCard> -->
+    <div class="d-flex products-list">
+      <div v-for="(product, index) in products" :key="index">
+        <ProductCard :product-spec="product" />
+      </div>
     </div>
   </div>
 </template>
@@ -41,5 +59,57 @@
 <script setup lang="ts">
 import Banner from '@/components/Banner.vue';
 import CategoryCard from '@/components/CategoryCard.vue';
-// import ProductCard from '@/components/ProductCard.vue';
+import ProductCard from '@/components/ProductCard.vue';
+import { ProductSpec } from '@types';
+import { fetchProduct } from '@/api';
 </script>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      products: [] as ProductSpec[],
+    };
+  },
+
+  async beforeMount() {
+    for (let i = 1; i < 5; i++) {
+      const product = await fetchProduct(i);
+      this.products.push(product.data);
+    }
+  },
+};
+</script>
+<style>
+.cards-mobile {
+  flex-wrap: nowrap;
+  justify-content: space-between;
+  gap: 2vh;
+}
+
+.products-list {
+  justify-content: space-evenly;
+  flex-wrap: wrap;
+}
+
+.products-list .product {
+  max-width: 25vh !important;
+}
+
+@media (max-width: 1940px) {
+  .cards-mobile {
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+  }
+}
+
+@media (max-width: 767px) {
+  .cards-mobile {
+    gap: 2vh;
+  }
+
+  .products-list .product {
+    max-width: 15vh !important;
+  }
+}
+</style>
