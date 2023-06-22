51c940b7792588b303a6fdcfd4cd4bb7a4c5db0b
Lucas Pinto <github@lucks.pt>
2023-06-22 14:46:00 +0100

    chore: do it for all cases where it uses focus-ring

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,7 @@ import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 // import 'primevue/resources/themes/bootstrap4-light-blue/theme.css';
+import '@/assets/themeoverride.css';
 import '@/assets/theme.css';
 import 'primevue/resources/primevue.min.css';
 import 'primeicons/primeicons.css';
