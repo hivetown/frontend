0b7748741216bfe6461ebdab707018099e465dc6
Lucas Pinto <github@lucks.pt>
2023-07-05 16:39:45 +0100

    chore: linting & fix

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,50 +1,49 @@
 <template>
-	<h1>Utilizadores</h1>
-	<b-tabs v-model="activeTab">
-	  <b-tab title="Consumidores" @click="activateFirstTab">
-		<Consumer></Consumer>
-	  </b-tab>
-	  <b-tab title="Produtores" @click="activateSecondTab">
-		<Producer></Producer>
-	  </b-tab>
-	</b-tabs>
-  </template>
-  
-  <script lang="ts">
-  import Consumer from '../components/Consumer.vue';
-  import Producer from '../components/Producer.vue';
-  
-  export default {
-	components: {
-	  Consumer,
-	  Producer,
-	},
-	data() {
-	  return {
-		activeTab: 0, // Índice da aba ativa
-	  };
-	},
-	mounted() {
-	  const lastActiveTab = localStorage.getItem('lastActiveTab');
-	  if (lastActiveTab !== null) {
-		this.activeTab = parseInt(lastActiveTab);
-	  }
-	},
-	methods: {
-	  activateFirstTab() {
-		this.activeTab = 0;
-		localStorage.setItem('lastActiveTab', this.activeTab.toString());
-	  },
-	  activateSecondTab() {
-		this.activeTab = 1;
-		localStorage.setItem('lastActiveTab', this.activeTab.toString());
-	  },
-	},
-  };
-  </script>
-  
-<style scoped>
+  <h1>Utilizadores</h1>
+  <b-tabs v-model="activeTab">
+    <b-tab title="Consumidores" @click="activateFirstTab">
+      <Consumer></Consumer>
+    </b-tab>
+    <b-tab title="Produtores" @click="activateSecondTab">
+      <Producer></Producer>
+    </b-tab>
+  </b-tabs>
+</template>
 
+<script lang="ts">
+import Consumer from '../components/Consumer.vue';
+import Producer from '../components/Producer.vue';
+
+export default {
+  components: {
+    Consumer,
+    Producer,
+  },
+  data() {
+    return {
+      activeTab: 0, // Índice da aba ativa
+    };
+  },
+  mounted() {
+    const lastActiveTab = localStorage.getItem('lastActiveTab');
+    if (lastActiveTab !== null) {
+      this.activeTab = parseInt(lastActiveTab);
+    }
+  },
+  methods: {
+    activateFirstTab() {
+      this.activeTab = 0;
+      localStorage.setItem('lastActiveTab', this.activeTab.toString());
+    },
+    activateSecondTab() {
+      this.activeTab = 1;
+      localStorage.setItem('lastActiveTab', this.activeTab.toString());
+    },
+  },
+};
+</script>
+
+<style scoped>
 h1 {
   font-family: 'DM Serif Display';
   text-align: center;
@@ -54,5 +53,4 @@ h1 {
   margin-left: 25px;
   margin-right: 25px;
 }
-
 </style>
