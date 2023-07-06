cad17958141e7e5660b224e6469f7af46ad4d49e
Lucas Pinto <github@lucks.pt>
2023-07-06 22:05:25 +0100

    chore: use new pagination on admin pages

--- src/components/Producer.vue
+++ src/components/Producer.vue
@@ -9,20 +9,18 @@
       aria-label="Loading Product specifications"
     />
   </div>
-  <div class="card-container" v-if="users">
-    <div v-for="idx in qtd" :key="idx">
+  <div class="card-container" v-if="!isLoading && producers">
+    <div v-for="(producer, idx) in producers.items" :key="idx">
       <b-card
         id="b-card"
-        :title="users.items[idx - 1]?.user?.name || ''"
+        :title="producer.user.name"
         :img-src="
-          users.items[idx - 1]?.user?.image?.url
-            ? users.items[idx - 1]?.user?.image?.url
-            : '../../public/semimagem.png'
+          producer.user.image ? producer.user.image.url : '/semimagem.png'
         "
         :img-alt="
-          users.items[idx - 1]?.user?.image
-            ? users.items[idx - 1]?.user?.image?.alt || 'sem imagem'
-            : 'sem imagem'
+          producer.user.image
+            ? producer.user.image.alt
+            : `Imagem de ${producer.user.name}`
         "
         img-top
         tag="article"
@@ -30,65 +28,92 @@
         class="mb-2"
       >
         <b-card-text>
-          {{ users['items'][idx - 1]['user']['email'] }} <br />
-          <p v-if="users.items[idx - 1].user.type === 'PRODUCER'">Produtor</p>
-          <p v-else>{{ users['items'][idx - 1]['user']['type'] }}</p>
-          <p v-if="users.items[idx - 1].deletedAt != null">Conta desativa</p>
+          {{ producer.user.email }} <br />
+          <p v-if="producer.user.type === 'PRODUCER'">Produtor</p>
+          <p v-else>{{ producer.user.type }}</p>
+          <p v-if="producer.deletedAt != null">Conta desativa</p>
         </b-card-text>
-
-        <b-button
-          :href="'admin/producer/' + users['items'][idx - 1]['user']['id']"
-          class="custom-button"
-          >Ver perfil</b-button
+        <PrimeButton @click="routeTo(`/admin/producer/${producer.user.id}`)"
+          >Ver perfil</PrimeButton
         >
       </b-card>
     </div>
   </div>
+
   <Pagination
-    :total-rows="totalItems"
-    :per-page="pageSize"
-    :current-page="page"
+    v-if="producers"
+    :items="producers"
+    :page-size="currentFilters.pageSizeP"
+    :page="currentFilters.pageP"
+    @page-change="onPageChange"
   >
   </Pagination>
 </template>
-
 <script setup lang="ts">
 import ProgressSpinner from 'primevue/progressspinner';
-import Pagination from '../components/Pagination.vue';
-import { getProducers, getProducersValues } from '../api/producers';
-import { onMounted, ref } from 'vue';
+import Pagination from '@/components/Pagination.vue';
+import { onBeforeMount, ref, watch } from 'vue';
 import { BaseItems, Producer } from '@/types';
+import { PageState } from 'primevue/paginator';
+import { useRoute } from 'vue-router';
+import PrimeButton from 'primevue/button';
+import { fetchAllProducers } from '@/api';
+import { useRouter } from 'vue-router';
+
 const isLoading = ref(true);
-const users = ref<BaseItems<Producer>>();
-const qtd = ref(0);
-const page = ref(1);
-const pageSize = ref(24);
-const totalItems = ref(0);
-onMounted(async () => {
-  try {
-    const responseItems = await getProducersValues();
-    page.value = responseItems.data.page;
-    isLoading.value = false; // Defina isLoading como falso para esconder o indicador de carregamento
-    const urlSearchParams = new URLSearchParams(window.location.search);
-    const pageParam = urlSearchParams.get('page');
-    if (pageParam) {
-      if (pageParam > responseItems.data.totalPages) {
-        page.value = responseItems.data.totalPages;
-      } else {
-        page.value = Number(pageParam);
-      }
-    }
-    pageSize.value = responseItems.data.pageSize;
-    totalItems.value = responseItems.data.totalItems;
-    const response = await getProducers(page.value, pageSize.value);
-    users.value = response.data;
-    qtd.value = Number(users.value?.items.length);
-  } catch (error) {
-    isLoading.value = false; // Certifique-se de definir isLoading como falso mesmo em caso de erro
-    console.error(error);
-  }
+const producers = ref<BaseItems<Producer>>();
+const route = useRoute();
+const router = useRouter();
+const currentFilters = ref({
+  pageC: Number(route.query.pageC) || 1,
+  pageSizeC: Number(route.query.pageSizeC) || 24,
+  pageP: Number(route.query.pageP) || 1,
+  pageSizeP: Number(route.query.pageSizeP) || 24,
 });
+
+watch(currentFilters.value, (newFilters) => {
+  router.push({
+    query: {
+      ...newFilters,
+    },
+  });
+});
+
+const loadProducers = async () => {
+  isLoading.value = true;
+
+  try {
+    const response = await fetchAllProducers({
+      page: currentFilters.value.pageP,
+      pageSize: currentFilters.value.pageSizeP,
+      includeAll: true,
+    });
+
+    producers.value = response.data;
+  } finally {
+    isLoading.value = false;
+  }
+};
+
+onBeforeMount(loadProducers);
+
+const onPageChange = async (page: Partial<PageState>) => {
+  if (page.page) {
+    currentFilters.value.pageP = page.page + 1;
+  }
+
+  if (page.rows) {
+    currentFilters.value.pageSizeP = page.rows;
+  }
+
+  await loadProducers();
+};
+
+const routeTo = (path: string) => {
+  router.push(path);
+};
 </script>
+
 <style scoped>
 .card-container {
   display: flex;
@@ -102,10 +127,5 @@ onMounted(async () => {
 #b-card {
   background-color: rgb(239, 243, 247);
   height: 400px;
-}
-.custom-button {
-  background-color: #e9dfdf; /* Cor de fundo personalizada */
-  color: #000000; /* Cor do texto personalizada */
-  /* Mais estilos personalizados aqui */
 }
 </style>
