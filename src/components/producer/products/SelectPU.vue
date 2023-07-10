043ba9748a2d53c467bde9f04ca50916863edbd3
BMGonzalez24 <bruno.mg.gonzalez@gmail.com>
2023-07-10 17:05:34 +0100

    .

--- src/components/producer/products/SelectPU.vue
+++ src/components/producer/products/SelectPU.vue
@@ -40,12 +40,6 @@
             >
               <template #value="slotProps">
                 <div v-if="slotProps.value" class="flex align-items-center">
-                  <img
-                    :alt="slotProps.value.label"
-                    src="https://primefaces.org/cdn/primevue/images/flag/flag_placeholder.png"
-                    :class="`mr-2 flag flag-${slotProps.value.code.toLowerCase()}`"
-                    style="width: 18px"
-                  />
                   <div>{{ slotProps.value.name }}</div>
                 </div>
                 <span v-else>
@@ -54,12 +48,6 @@
               </template>
               <template #option="slotProps">
                 <div class="flex align-items-center">
-                  <img
-                    :alt="slotProps.option.label"
-                    src="https://primefaces.org/cdn/primevue/images/flag/flag_placeholder.png"
-                    :class="`mr-2 flag flag-${slotProps.option.code.toLowerCase()}`"
-                    style="width: 18px"
-                  />
                   <div>{{ slotProps.option.name }}</div>
                 </div>
               </template>
@@ -227,6 +215,7 @@ export default {
           code: productionUnit.items.value.items[i].id.toString(),
         });
       }
+      selectedPU.value.values = optsPUs.value[0];
     };
 
     // Apply the selected product to the form
